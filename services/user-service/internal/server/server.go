4617fa4065d9690fcd5e5acfeb2b4173109dffa9
CHIFendse <timgranden@yandex.ru>
2026-05-30 17:10:45 +0300

    fix

--- services/user-service/internal/server/server.go
+++ services/user-service/internal/server/server.go
@@ -19,12 +19,9 @@ func New(db *sql.DB) *User {
 func (u *User) Register(ctx context.Context, req *userpb.RegisterRequest) (*userpb.RegisterResponse, error) {
 	var id string
 	id = uuid.New().String()
-	err := u.db.QueryRowContext(ctx,
+	u.db.QueryRowContext(ctx,
 		`INSERT INTO users (user_id, username, email, phone) VALUES ($1, $2, $3, $4)`,
 		id, req.Username, req.Email, req.Phone,
-	).Scan(&id)
-	if err != nil {
-		return nil, err
-	}
+	)
 	return &userpb.RegisterResponse{UserId: id}, nil
 }
